03ff81080a27ac5d3340a9f96f12cc03dc2731ef
kimchi <kim24daisuki@gmail.com>
2024-12-23 12:11:16 +0700

    menulist

--- Client/src/components/common/Header.jsx
+++ Client/src/components/common/Header.jsx
@@ -1,4 +1,5 @@
-import { Container } from "./Design";
+import { Container, CustomNavLinkList } from "./Design";
+import { menulists } from "../../assets/Data";
 
 export const Header = () => {
   return (
@@ -6,13 +7,26 @@ export const Header = () => {
       <header className="header py-1 bg-blue-900 text-white">
         <Container>
           <nav className="p-4 flex justify-between items-center relative">
-            <div className="flex items-center gap-14">
-              <div>
-                <img src="../image/common/auction_32px.png" alt="Logo" className="h-11" />
+            <div className="flex items-center gap-8">
+              <div className="flex items-center gap-4">
+                <img
+                  src="../image/common/camping_logo.png"
+                  alt="Logo"
+                  className="h-11"
+                />
+                <h1 className="text-lg font-bold hover:text-red-500 transition-all">Car Auction</h1>
               </div>
-              <div className="text-lg font-bold">
-                <h1>Car Auction</h1>
-              </div>
+
+              {/* Menu List */}
+              <ul className="hidden lg:flex items-center justify-between gap-8">
+                {menulists.map((list, index) => (
+                  <li key={index} className="capitalize list-none">
+                    <CustomNavLinkList href={list.path}>
+                      {list.link}
+                    </CustomNavLinkList>
+                  </li>
+                ))}
+              </ul>
             </div>
             <div>
               <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-all">
